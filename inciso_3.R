eb5f16bc85f112c61c5c5b2eb0dc6f703359604c
franco-nunez <73083742+franco-nunez@users.noreply.github.com>
2020-12-16 01:02:44 -0300

    Update inciso_3.R

--- inciso_3.R
+++ inciso_3.R
@@ -69,6 +69,8 @@ pars.S
 source("PS3_SVAR_Analysis_LR.R")
 source("PS3_SVAR_Bootstrap_LR.R")
 source("PS3_SVAR_Plots_LR.R")
+source("SVAR_SIRF_m.R")
+source("SIRF_transform.R")
 
 H <- 12 # Horizon
 a <- 0.95 # Confidence level
@@ -77,18 +79,34 @@ R <- 500 # No. of bootstrap replications
 # Bootstrap replications
 Yb <- boot.rb.replicate(VAR, Yd0, pmax, R)
 
+
 # IRF (bootstrap)
-SVAR.SIRF.boot.LR <- SVAR.sirf.boot.LR(SVAR, Yb, pmax, H, a, R)
-plot.sirf.boot(SVAR.SIRF.boot.LR, m, H)
+#SVAR.SIRF.boot.LR <- SVAR.sirf.boot.LR(SVAR, Yb, pmax, H, a, R)
+#plot.sirf.boot(SVAR.SIRF.boot.LR, m, H)
+
+# IRF (bootstrap with modified file)
+SVAR.SIRF.boot.LR2 <- SVAR.sirf.boot.LR.modified(SVAR, Yb, pmax, H, a, R)
+plot.sirf.boot(SVAR.SIRF.boot.LR2, m, H)
+
 
 # Cumulative IRF (bootstrap)
-SVAR.SIRF.c.boot.LR <- SVAR.sirf.boot.LR(SVAR, Yb, pmax, H, a, R, cumulative = TRUE)
-plot.sirf.boot(SVAR.SIRF.c.boot.LR, m, H)
+#SVAR.SIRF.c.boot.LR <- SVAR.sirf.boot.LR(SVAR, Yb, pmax, H, a, R, cumulative = TRUE)
+#plot.sirf.boot(SVAR.SIRF.c.boot.LR, m, H)
+
+# Cumulative IRF (bootstrap with modified file)
+SVAR.SIRF.c.boot.LR2 <- SVAR.sirf.boot.LR.modified(SVAR, Yb, pmax, H, a, R, cumulative = TRUE)
+plot.sirf.boot(SVAR.SIRF.c.boot.LR2, m, H)
+
 
 # FEVD (bootstrap)
 SVAR.FEVD.boot.LR <- SVAR.fevd.boot.LR(SVAR, Yb, pmax, H, a, R)
 plot.fevd.boot(SVAR.FEVD.boot.LR, m, H)
 
 # HD
-SVAR.HD <- SVAR.hd(SVAR)
-plot.hd(Y.d, SVAR.HD, m, pmax)
+#SVAR.HD <- SVAR.hd(SVAR)
+#plot.hd(Y.d, SVAR.HD, m, pmax)
+
+
+# SVAR analysis: transformed IRF for inflation (TBC) ####
+SVAR.SIRF.c.boot.LR.inflation <- trans.pw.boot(operator = "-", I = SVAR.SIRF.c.boot.LR2$pe, vi=3, vj=2, sk=3, boot = SVAR.SIRF.c.boot.LR2$boot, a)
+plot(SVAR.SIRF.c.boot.LR2$boot)
